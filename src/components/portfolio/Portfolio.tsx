7cb03ce7492d4e80d2ec6a2a451c8fbf8f3fb98d
aymane taoufyq <skays.dev@gmail.com>
2023-10-23 16:58:24 +0100

    go

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -2,14 +2,17 @@ import { useEffect, useState, FC } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import Image from "next/image";
 import ModalMain from "./modal/ModalMain";
-import { getProjects } from "@/services/projects";
+import { getProjectCategory, getProjects } from "@/services/projects";
 
 const Portfolio: FC = (): JSX.Element => {
   const [getModal, setGetModal] = useState(false);
   const [modalId, setModalId] = useState(1);
   const [slug, setSlug] = useState("");
+  const [category, setCategory] = useState("");
+
   // States Of Data Fetch it
   const [allProjects, setAllProjects] = useState<any[]>([]);
+  const [categoryProjects, setCategoryProject] = useState<any[]>([]);
 
   const handleModal = (id: number, slug: string): void => {
     setGetModal(true);
@@ -39,6 +42,23 @@ const Portfolio: FC = (): JSX.Element => {
     // we call the function for run the code
     fetchRecentBlogs();
   }, []);
+
+  useEffect(() => {
+    // we create a function for fetch the data and set it in useState and call it @fetchRecentBlogs
+    const fetchRecentBlogs = async () => {
+      try {
+        let categoryProjects = await getProjectCategory(category);
+        setCategoryProject(categoryProjects);
+      } catch (error) {
+        console.error(`Error fetching recent blogs: ${error}`);
+      }
+    };
+    // end function
+
+    // we call the function for run the code
+    fetchRecentBlogs();
+  }, [category]);
+
   return (
     <>
       {allProjects ? <>
@@ -46,20 +66,20 @@ const Portfolio: FC = (): JSX.Element => {
           <Tabs>
             <TabList className="portfolio-tab-list" data-aos="fade-up">
               <Tab>ALL</Tab>
-              <Tab>Front end</Tab>
-              <Tab>Back end</Tab>
-              <Tab>UI/UX</Tab>
-              <Tab>ALX Tasks</Tab>
+              <Tab onClick={() => setCategory('front-end')}>Front end</Tab>
+              <Tab onClick={() => setCategory('back-end')}>Back end</Tab>
+              <Tab onClick={() => setCategory('ui-ux')}>UI/UX</Tab>
+              <Tab onClick={() => setCategory('alx')}>ALX Tasks</Tab>
             </TabList>
 
             <div className="container">
               <TabPanel>
+                <div className="tab-container">
+                  {allProjects.length > 0 ? allProjects.map((item) => {
+                    let delayAnimation = -100;
+                    const { slug, image, typeProject, typeCard } = item.node;
+                    return (
 
-                {allProjects ? allProjects.map((item) => {
-                  let delayAnimation = -100;
-                  const { slug, image, typeProject, typeCard } = item.node;
-                  return (
-                    <div className="tab-container">
                       <div
                         key={slug}
                         data-aos="fade-right"
@@ -75,184 +95,161 @@ const Portfolio: FC = (): JSX.Element => {
                           </h3>
                         </div>
                       </div>
-                    </div>
-                  );
-                }) :
-                  <div
-                    data-aos="fade-right"
-                    data-aos-delay='100'
-                  >
-                    <h1 style={{ textAlign: 'center' }}>not Found</h1>
-                  </div>
-                }
 
-              </TabPanel>
-
-              <TabPanel>
-
-                {allProjects.map((item) => {
-                  const { categories } = item.node;
-                  const frontEndCategories = categories.filter((category: any) => category.slug === "front-end");
-
-                  if (frontEndCategories.length > 0) {
-                    let delayAnimation = -100;
-                    const { slug, image, typeProject, typeCard } = item.node;
-
-                    return (
-                      <div className="tab-container">
-                        <div
-                          key={slug}
-                          data-aos="fade-right"
-                          data-aos-delay={delayAnimation + 100}
-                        >
-                          <div
-                            className="tab-content"
-                            onClick={() => handleModal(typeCard, slug)}
-                          >
-                            <Image src={image.url} alt="portfolio project demo" width={895} height={552} />
-                            <h3>
-                              <span className="content-title">{typeProject}</span>
-                            </h3>
-                          </div>
-                        </div>
-                      </div>
                     );
-                  }
-                  return (
+                  }) :
                     <div
                       data-aos="fade-right"
                       data-aos-delay='100'
                     >
                       <h1 style={{ textAlign: 'center' }}>not Found</h1>
                     </div>
-                  );
-                })}
+                  }
+                </div>
 
               </TabPanel>
 
               <TabPanel>
-
-                {allProjects.map((item) => {
-                  const { categories } = item.node;
-                  const frontEndCategories = categories.filter((category: any) => category.slug === "back-end");
-
-                  if (frontEndCategories.length > 0) {
-                    let delayAnimation = -100;
+                <div className="tab-container">
+                  {categoryProjects.length > 0 ? categoryProjects.map((item) => {
                     const { slug, image, typeProject, typeCard } = item.node;
-
+                    let delayAnimation = -100;
                     return (
-                      <div className="tab-container">
+                      <div
+                        key={slug}
+                        data-aos="fade-right"
+                        data-aos-delay={delayAnimation + 100}
+                      >
                         <div
-                          key={slug}
-                          data-aos="fade-right"
-                          data-aos-delay={delayAnimation + 100}
+                          className="tab-content"
+                          onClick={() => handleModal(typeCard, slug)}
                         >
-                          <div
-                            className="tab-content"
-                            onClick={() => handleModal(typeCard, slug)}
-                          >
-                            <Image src={image.url} alt="portfolio project demo" width={895} height={552} />
-                            <h3>
-                              <span className="content-title">{typeProject}</span>
-                            </h3>
-                          </div>
+                          <Image src={image.url} alt="portfolio project demo" width={895} height={552} />
+                          <h3>
+                            <span className="content-title">{typeProject}</span>
+                          </h3>
                         </div>
                       </div>
+
                     );
-                  }
-                  return (
+                  }) :
                     <div
                       data-aos="fade-right"
                       data-aos-delay='100'
                     >
                       <h1 style={{ textAlign: 'center' }}>not Found</h1>
                     </div>
-                  );
-                })}
-
+                  }
+                </div>
               </TabPanel>
 
               <TabPanel>
-
-                {allProjects.map((item) => {
-                  const { categories } = item.node;
-                  const frontEndCategories = categories.filter((category: any) => category.slug === "ui-ux");
-
-                  if (frontEndCategories.length > 0) {
-                    let delayAnimation = -100;
+                <div className="tab-container">
+                  {categoryProjects.length > 0 ? categoryProjects.map((item) => {
                     const { slug, image, typeProject, typeCard } = item.node;
-
+                    let delayAnimation = -100;
                     return (
-                      <div className="tab-container">
+
+                      <div
+                        key={slug}
+                        data-aos="fade-right"
+                        data-aos-delay={delayAnimation + 100}
+                      >
                         <div
-                          key={slug}
-                          data-aos="fade-right"
-                          data-aos-delay={delayAnimation + 100}
+                          className="tab-content"
+                          onClick={() => handleModal(typeCard, slug)}
                         >
-                          <div
-                            className="tab-content"
-                            onClick={() => handleModal(typeCard, slug)}
-                          >
-                            <Image src={image.url} alt="portfolio project demo" width={895} height={552} />
-                            <h3>
-                              <span className="content-title">{typeProject}</span>
-                            </h3>
-                          </div>
+                          <Image src={image.url} alt="portfolio project demo" width={895} height={552} />
+                          <h3>
+                            <span className="content-title">{typeProject}</span>
+                          </h3>
                         </div>
                       </div>
+
                     );
-                  }
-                  return (
+                  }) :
                     <div
                       data-aos="fade-right"
                       data-aos-delay='100'
                     >
                       <h1 style={{ textAlign: 'center' }}>not Found</h1>
                     </div>
-                  );
-                })}
+                  }
+                </div>
 
               </TabPanel>
 
               <TabPanel>
 
-                {allProjects.map((item) => {
-                  const { categories } = item.node;
-                  const frontEndCategories = categories.filter((category: any) => category.slug === "alx");
-
-                  if (frontEndCategories.length > 0) {
-                    let delayAnimation = -100;
+                <div className="tab-container">
+                  {categoryProjects.length > 0 ? categoryProjects.map((item) => {
                     const { slug, image, typeProject, typeCard } = item.node;
-
+                    let delayAnimation = -100;
                     return (
-                      <div className="tab-container">
+
+                      <div
+                        key={slug}
+                        data-aos="fade-right"
+                        data-aos-delay={delayAnimation + 100}
+                      >
                         <div
-                          key={slug}
-                          data-aos="fade-right"
-                          data-aos-delay={delayAnimation + 100}
+                          className="tab-content"
+                          onClick={() => handleModal(typeCard, slug)}
                         >
-                          <div
-                            className="tab-content"
-                            onClick={() => handleModal(typeCard, slug)}
-                          >
-                            <Image src={image.url} alt="portfolio project demo" width={895} height={552} />
-                            <h3>
-                              <span className="content-title">{typeProject}</span>
-                            </h3>
-                          </div>
+                          <Image src={image.url} alt="portfolio project demo" width={895} height={552} />
+                          <h3>
+                            <span className="content-title">{typeProject}</span>
+                          </h3>
                         </div>
                       </div>
+
                     );
-                  }
-                  return (
+                  }) :
                     <div
                       data-aos="fade-right"
                       data-aos-delay='100'
                     >
                       <h1 style={{ textAlign: 'center' }}>not Found</h1>
                     </div>
-                  );
-                })}
+                  }
+                </div>
+
+              </TabPanel>
+
+              <TabPanel>
+
+                <div className="tab-container">
+                  {categoryProjects.length > 0 ? categoryProjects.map((item) => {
+                    const { slug, image, typeProject, typeCard } = item.node;
+                    let delayAnimation = -100;
+                    return (
+
+                      <div
+                        key={slug}
+                        data-aos="fade-right"
+                        data-aos-delay={delayAnimation + 100}
+                      >
+                        <div
+                          className="tab-content"
+                          onClick={() => handleModal(typeCard, slug)}
+                        >
+                          <Image src={image.url} alt="portfolio project demo" width={895} height={552} />
+                          <h3>
+                            <span className="content-title">{typeProject}</span>
+                          </h3>
+                        </div>
+                      </div>
+
+                    );
+                  }) :
+                    <div
+                      data-aos="fade-right"
+                      data-aos-delay='100'
+                    >
+                      <h1 style={{ textAlign: 'center' }}>not Found</h1>
+                    </div>
+                  }
+                </div>
 
               </TabPanel>
             </div>
--- src/services/projects.ts
+++ src/services/projects.ts
@@ -8,7 +8,7 @@ export const getProjects = async (): Promise<any> => {
         const response = await axios.post(graphqlAPI || 'https://api-eu-west-2.hygraph.com/v2/clnqjgbp1gus401uh2ml3bid8/master', {
             query: `
             query projects {
-                projectsConnection {
+                projectsConnection(orderBy: createdAt_DESC) {
                     edges {
                         node {
                             categories {
@@ -37,6 +37,60 @@ export const getProjects = async (): Promise<any> => {
                 }
             }
         `,
+        }, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data.data.projectsConnection.edges;
+        } else {
+            throw new Error('Network response was not ok');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+export const getProjectCategory = async (slug: string): Promise<any> => {
+    try {
+        const response = await axios.post(graphqlAPI || 'https://api-eu-west-2.hygraph.com/v2/clnqjgbp1gus401uh2ml3bid8/master', {
+            query: `
+            query getCategoryProject($slug: String!) {
+                projectsConnection(orderBy: createdAt_DESC ,where: { categories_some: { slug: $slug } }) {
+                    edges {
+                        node {
+                            categories {
+                                slug
+                            }
+                            createdAt
+                            slug
+                            image {
+                                url
+                            }
+                            image2 {
+                                url
+                            }
+                            image3 {
+                                url
+                            }
+                            linkYoutube
+                            languages
+                            link
+                            preview
+                            projectName
+                            typeCard
+                            typeProject
+                        }
+                    }
+                }
+            }
+        `,
+            variables: {
+                slug: slug,
+            }
         }, {
             headers: {
                 "Content-Type": "application/json",
