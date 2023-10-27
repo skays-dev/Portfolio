e6b56f52ac6a7fa85b2f9adacf07d3098fc519ac
aymane taoufyq <skays.dev@gmail.com>
2023-10-27 23:32:06 +0100

    go

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -4,7 +4,6 @@ import Education from "./Education";
 import Experience from "./Experience";
 import PersonalInfo from "./PersonalInfo";
 import Skills from "./Skills";
-import cv from "../../../public/assets/img/cv.webp";
 import heroImgMobile from "../../../public/assets/img/hero/img-mobile.jpg";
 import Image from "next/image";
 
@@ -39,7 +38,7 @@ const index: FC = (): JSX.Element => {
               {/* End personal info */}
 
               <div className="col-12 mt-1">
-                <a className="button" href="#" download>
+                <a className="button" href="assets/img/cv.pdf" download>
                   <span className="button-text">Download CV</span>
                   <span className="button-icon fa fa-download"></span>
                 </a>
