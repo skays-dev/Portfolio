abe98b7e8d4a583fc22aae28b326eff28c379a71
aymane taoufyq <111222157+skays-dev@users.noreply.github.com>
2024-02-21 20:02:38 +0100

    Update Education.tsx

--- src/components/about/Education.tsx
+++ src/components/about/Education.tsx
@@ -3,23 +3,22 @@ import { FC } from "react";
 
 const educationContent = [
   {
-    year: "   2023 - Present",
-    degree: "CERTIFICATE",
-    institute: "ALX Software Engineering",
-    details: `ALX is a leading technology training provider, built to accelerate the careers of young Africans through the technology and professional skills that enable them to thrive in the digital economy.`,
-    link: "https://www.alxafrica.com/"
+    year: "2023 - Present",
+    degree: "",
+    institute: "International University of Rabat",
+    details: `Specialization: Full Stack Java/Spring-Boot/Angular Program`,
   },
   {
     year: "2021 - 2023",
     degree: "Technicien Specialise",
     institute: "ISTA Development digital option full stack",
-    details: `Founded in 1974, OFPPT is a public training institution. It offers vocational training free of charge. The training is based on a demand-driven strategy meeting the qualification needs and requirements of the labor market. OFPPT has a large network of training centers all over Morocco. These centers provide acknowledged competencies at the service of business companies.`,
+    details: `Specialization: digital developement option full stack`,
   },
   {
     year: "2020 - 2021",
     degree: "Baccalaureate",
-    institute: "Baccalaureate in Physical Sciences",
-    details: `Baccalaureate in Physical Sciences graduate with a solid foundation in physics and chemistry, complemented by hands-on laboratory experience. Skilled in critical thinking and problem-solving within the realm of physical sciences.`,
+    institute: "Baccalaureate in Physical Sciences option french",
+    details: ``,
   },
 ];
 
